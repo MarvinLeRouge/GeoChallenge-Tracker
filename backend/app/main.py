e6b9ff91e27261c6a389817755395119de93cdf7
Jean Ceugniet <jean.ceugniet@gmail.com>
2025-08-15 09:47:15 +0200

    Seeding modification + name normalization
    backend/app/db/seed.py -> backend/app/db/seed_data.py
        better naming
    backend/app/db/seed_indexes.py
        ensure indexes
    backend/app/main.py
        seed indexes on stratup. Seed data in DEV if needed.
    backend/app/models/cache.py
        renamed fields to avoid confusion
    backend/app/models/attribute.py -> backend/app/models/cache_attribute.py
        consistent naming
    backend/app/models/cache_size.py
        better fields naming
        added field order
    backend/app/models/cache_type.py
        added field aliases
    backend/data/seeds/cache_attributes.json
        consistent naming between seeds files and models
    backend/data/seeds/cache_sizes.json
        consistent naming between seeds files and models
    backend/data/seeds/cache_types.json
        consistent naming between seeds files and models
    backend/requirements.txt
        version specific modules between passlib and bcrypt to avoid conflicts (not about seeding)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 # backend/app/main.py
 
+import os
 from fastapi import FastAPI
 from app.api.routes import routers
+from app.db.seed_indexes import ensure_indexes
+from app.db.seed_data import seed_referentials
 
 app = FastAPI(title="GeoChallenge API")
+
+@app.on_event("startup")
+def _startup():
+    ensure_indexes()  # toujours
+
+    if os.getenv("SEED_ON_STARTUP", "false").lower() == "true":
+        seed_referentials()  # idempotent et léger uniquement
 
 # Inclusion des routes
 for r in routers:
