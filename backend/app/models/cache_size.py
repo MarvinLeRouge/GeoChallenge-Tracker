3efc5bf031291edcf7c4efea94033b1ba2f0ce40
Jean Ceugniet <jean.ceugniet@gmail.com>
2025-08-16 14:16:21 +0200

    Total models revision

--- backend/app/models/cache_size.py
+++ backend/app/models/cache_size.py
@@ -1,29 +1,25 @@
 # backend/app/api/models/cache_size.py
 
-from pydantic import BaseModel, Field
+from __future__ import annotations
 from typing import Optional
 import datetime as dt
+from pydantic import BaseModel, Field
 from app.core.utils import *
 from app.core.bson_utils import *
 
 class CacheSizeBase(BaseModel):
     name: str                    # ex: "Small", "Micro", "Regular"
     code: Optional[str] = None   # code interne ou abbr (ex: "S", "M")
-    order: Optional[int] = None   # ordonnancement des cache sizes
+    order: Optional[int] = None  # ordonnancement des cache sizes
 
 class CacheSizeCreate(CacheSizeBase):
     pass
 
 class CacheSizeUpdate(BaseModel):
-    name: Optional[str]
-    code: Optional[str]
-    order: Optional[int]
+    name: Optional[str] = None
+    code: Optional[str] = None
+    order: Optional[int] = None
 
-class CacheSize(CacheSizeBase):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+class CacheSize(MongoBaseModel, CacheSizeBase):
     created_at: dt.datetime = Field(default_factory=lambda: now())
     updated_at: Optional[dt.datetime] = None
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {PyObjectId: str}
